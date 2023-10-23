3744ccbcc85838fbd499c3d03dd69b7e3e473d7e
Radu Ranzascu <radustefan3010@gmail.com>
2023-10-23 15:03:25 +0300

    add dotenv

--- src/api/fetchWeather.ts
+++ src/api/fetchWeather.ts
@@ -3,7 +3,9 @@ import { Weather } from '../types';
 
 export const fetchWeatherData = async (location: string): Promise<Weather> => {
   const response = await axios.get(
-    `https://api.weatherapi.com/v1/forecast.json?key=02043fb233904559812103820220610&q=${location}&days=7&aqi=yes&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${
+      import.meta.env.VITE_SECRET_KEY
+    }&q=${location}&days=7&aqi=yes&alerts=no`
   );
   return response.data;
 };
