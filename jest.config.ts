90335b1e20552ced488759e5e94af86cd5b01d8f
Radu Ranzascu <radustefan3010@gmail.com>
2023-11-01 18:02:31 +0200

    add sonar CI for test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jest-environment-jsdom',
-  coverageReporters: ['text'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
